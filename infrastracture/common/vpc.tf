1085f0226317049e2838a02ad963bd0182fe609e
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-08-22 00:56:35 +0200

    fix vpc name

--- infrastracture/common/vpc.tf
+++ infrastracture/common/vpc.tf
@@ -1,9 +1,17 @@
 resource "aws_vpc" "prod" {
   provider = aws.prod
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod"
+  }
 }
 
 resource "aws_vpc" "dev" {
   provider = aws.dev
   cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "dev"
+  }
 }
