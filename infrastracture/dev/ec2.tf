59a2e416a322839bc4d06283374dafcbd13ff5c1
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-08-22 15:51:49 +0200

    attach key pair to ec2 instance

--- infrastracture/dev/ec2.tf
+++ infrastracture/dev/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "app_server" {
 
   ami = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
+  key_name = aws_key_pair.app.key_name
 
   tags = {
     Name = "app-server"
---
+++ infrastracture/dev/key_pair.tf
@@ -0,0 +1,4 @@
+resource "aws_key_pair" "app" {
+  key_name = "app"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCbdzPgncalqD4IRqPs5ft+m1xifp6rnyKW3t6ZfB7fGp8A2ccmGE7oM2L4dwkCX69lyE+bHISoKrKGBJNiLkxlyASyRQ/z5VYC6ODfdPHn5IUgn7X0rIIve0zwqDkl0f5lkiJVHWsUekroQKVQ2mXTUczDVBPd5DRHt28AWLk3YYkJHGUkFQ/CNTKWx9LoGqOwCoMAZOJW9FjC15etV3vF/OsylgW/Vetgs3wlJjhBhkgXnOuFEC0FJyvUfHychgDWq4Uh8vTyLZwBM5VP4Hk/k91tFM6n+pMztjXyUdIWkpO23TB8VOSi6DvAcyoehQZu9eP75n7BQe8AkIzZGWG3"
+}
