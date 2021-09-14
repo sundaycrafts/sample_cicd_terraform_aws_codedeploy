fb6f5b739c0abdf77e592122dc8aeb0aea3e542c
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-09-15 00:18:23 +0200

    refactor: remove redundant iam policies

--- infrastracture/common/iam_role.tf
+++ infrastracture/common/iam_role.tf
@@ -1,4 +1,5 @@
-// used for service role for codedeploy https://docs.aws.amazon.com/codedeploy/latest/userguide/tutorials-wordpress-deploy-application.html
+// used for service role for CodeDeploy (not its agent)
+// see https://docs.aws.amazon.com/codedeploy/latest/userguide/tutorials-wordpress-deploy-application.html
 resource "aws_iam_role" "codedeploy_service_role" {
   name = "CodeDeployServiceRole"
 
@@ -26,9 +27,8 @@ resource "aws_iam_role_policy_attachment" "codedeploy" {
   policy_arn = data.aws_iam_policy.AWSCodeDeployRole.arn
 }
 
-// used for codedeploy agent in EC2 instance https://docs.aws.amazon.com/codedeploy/latest/userguide/getting-started-create-iam-instance-profile.html
-// and for installing codedeploy agent via ssm https://docs.aws.amazon.com/codedeploy/latest/userguide/codedeploy-agent-operations-install-ssm.html
-// see also https://docs.aws.amazon.com/systems-manager/latest/userguide/setup-instance-profile.html
+// the statement and assume statement is for CodeDeploy agent in EC2 instance
+// see https://docs.aws.amazon.com/codedeploy/latest/userguide/getting-started-create-iam-instance-profile.html
 resource "aws_iam_role" "codedeploy_ec2_instance_profile" {
   name = "codedeploy_ec2_instance_profile"
 
@@ -40,28 +40,13 @@ resource "aws_iam_role" "codedeploy_ec2_instance_profile" {
   "Statement": [
     {
         "Effect": "Allow",
-        "Action": "s3:GetObject",
-        "Resource": [
-            "arn:aws:s3:::aws-ssm-${var.region_dev}/*",
-            "arn:aws:s3:::aws-windows-downloads-${var.region_dev}/*",
-            "arn:aws:s3:::amazon-ssm-${var.region_dev}/*",
-            "arn:aws:s3:::amazon-ssm-packages-${var.region_dev}/*",
-            "arn:aws:s3:::region-birdwatcher-prod/*",
-            "arn:aws:s3:::aws-ssm-distributor-file-${var.region_dev}/*",
-            "arn:aws:s3:::aws-ssm-document-attachments-${var.region_dev}/*",
-            "arn:aws:s3:::patch-baseline-snapshot-${var.region_dev}/*"
-        ]
-    },
-    {
-        "Effect": "Allow",
         "Action": [
-            "s3:GetObject",
-            "s3:PutObject",
-            "s3:PutObjectAcl",
-            "s3:GetEncryptionConfiguration"
+            "s3:Get*",
+            "s3:List*"
         ],
         "Resource": [
-          "${aws_s3_bucket.codedeploy_asset.arn}"
+          "${aws_s3_bucket.codedeploy_asset.arn}",
+          "arn:aws:s3:::aws-codedeploy-${var.region_dev}/*"
         ]
     }
   ]
@@ -86,6 +71,7 @@ EOF
 EOF
 }
 
+# Attach policy to enable an instance to use SSM core functionality
 resource "aws_iam_role_policy_attachment" "codedeploy_ec2_instance_profile" {
   role = aws_iam_role.codedeploy_ec2_instance_profile.name
   policy_arn = data.aws_iam_policy.AmazonSSMManagedInstanceCore.arn
