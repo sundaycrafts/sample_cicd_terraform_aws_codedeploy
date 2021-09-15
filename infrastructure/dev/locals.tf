fcc1559712fa901cef653638d0adcdcab08438d9
sundaycrafts <sundaycrafts@users.noreply.github.com>
2021-09-15 03:20:02 +0200

    add comment to tf file

--- infrastructure/dev/locals.tf
+++ infrastructure/dev/locals.tf
@@ -1,6 +1,6 @@
 locals {
   ami = {
-    ubuntu20042lts = "ami-09e67e426f25ce0d7"
-    ubuntucustomnode = "ami-01c213824d7e9a151"
+    ubuntu20042lts = "ami-09e67e426f25ce0d7" // official ami
+    ubuntucustomnode = "ami-01c213824d7e9a151" // custom ami support node.js and pm2
   }
 }
